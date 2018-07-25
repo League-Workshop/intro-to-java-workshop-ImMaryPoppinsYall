23fc0910bc887eb2f89347351436072d3b453135
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-25 11:56:34 -0700

    Day3

---
+++ src/section3/NiceProgram.java
@@ -0,0 +1,10 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NiceProgram {
+public static void main(String[] args) {
+System.out.println("nice program!");
+JOptionPane.showMessageDialog(null, "nice program!");
+}
+}
