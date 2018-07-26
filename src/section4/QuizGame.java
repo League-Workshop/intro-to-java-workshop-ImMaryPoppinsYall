5965e02e0752f0918ac1ce8dbbe15475c4a71975
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-26 12:01:51 -0700

    Day4

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,57 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String answer = JOptionPane.showInputDialog("What did Star-Lord do to distract Ronan from destroying the universe?");
 		// 3.  Use an if statement to check if their answer is correct
+		if (answer.equalsIgnoreCase("He challenges him in a dance off")) {
+			score = score + 1;
 		
 		// 4.  if the user's answer is correct
-		
+			JOptionPane.showMessageDialog(null, "Correct! 1 Point");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
 		// -- add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		answer = JOptionPane.showInputDialog("What was the name of Star-Lord's mom?");
+		if (answer.equalsIgnoreCase("Meredith Quill")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "Correct! 1 Point");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
+		answer = JOptionPane.showInputDialog("Who always makes a cameo in every Marvel movie?");
+		if (answer.equalsIgnoreCase("Stan Lee")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "Correct! 1 Point");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
+		answer = JOptionPane.showInputDialog("Who makes an appearance during the battle in Captain America: Civil War?");
+		if (answer.equalsIgnoreCase("Spider-Man")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "Correct! 1 Point");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
+		answer = JOptionPane.showInputDialog("What happened after Thanos snapped his fingers with the Infinity Gaunlet?");
+		if (answer.equalsIgnoreCase("Everyone dies")) {
+			score = score + 1;
+			JOptionPane.showMessageDialog(null, "Correct! 1 Point");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
 		
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showMessageDialog(null, "You got " + score + " out of 5");
 	}
 }
--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -16,45 +16,58 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+	Robot ks2o = new Robot("mini");
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+		ks2o.setAngle(0);
+		ks2o.microMove(10);
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		ks2o.setAngle(180);
+		ks2o.microMove(10);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+		ks2o.setAngle(270);
+		ks2o.microMove(10);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		ks2o.setAngle(90);
+		ks2o.microMove(10);
 	}
 
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		int robotXLocation = ks2o.getX();
+		int robotYLocation = ks2o.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		System.out.println("robotX "+robotXLocation);
+		System.out.println("robotY" +robotYLocation);
 		// 7. If robot is at same location as the little girl
 		//      --make a pop-up tell the robot where to go next
-		
+		if(robotXLocation == 720 && robotYLocation == 400) {
+			JOptionPane.showMessageDialog(null, "Go to the parrot on the left");
+		}
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
-		
+		if(robotXLocation == 590 && robotYLocation == 150) {
+			JOptionPane.showMessageDialog(null, "Go to the skull's eye");
+		}
 		// 9.  If the robot is in the final location
 		//     --call the treasureFound() method
-		
+		if(robotXLocation == 200 && robotYLocation == 60) {
+			JOptionPane.showMessageDialog(null, "You found the treasure!");
+		}
+	
 	}
+	
 
 	private void go() {
 		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
